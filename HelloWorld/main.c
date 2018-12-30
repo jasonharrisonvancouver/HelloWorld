3f0441c5a091287ccbad6b5695ee3e246f65429e
Jason Harrison <jasonharrisonvancouver@gmail.com>
2018-12-29 17:29:45 -0800

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by jason harrison on 2018-12-29.
+//  Copyright © 2018 jason harrison. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
